fcb48b7bd8dc81fc1fb28b1e6aafb950ca7793ba
Christophe Fauchard <christophe.fauchard@gmail.com>
2016-10-04 15:11:02 +0200

    - use admin css files, base.html, base_site.html - add forms.py - add form for Connection creation/modification

--- django/connection/views.py
+++ django/connection/views.py
@@ -8,6 +8,13 @@ from django.shortcuts import redirect
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
 
+
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404
+
+from .forms import ConnectionForm
+
 @login_required
 def detail(request, connection_name):
     connection_selected = (Connection.objects.get(name=connection_name))
@@ -15,19 +22,24 @@ def detail(request, connection_name):
     if permission_test(request, connection_selected) == False:
         return redirect('%s?next=%s' % (settings.LOGIN_URL, request.path))
 
-    context = {
-        'connection': connection_selected,
-    }
-    return render(request, 'connection/detail.html', context)
+    connection_selected = get_object_or_404(Connection, pk=connection_name)
+
+    if request.method == 'POST':
+        form = ConnectionForm(request.POST, instance = connection_selected)
+        if form.is_valid():
+            connection_selected = form.save(commit=False)
+            # post.author = request.user
+            # post.published_date = timezone.now()
+            connection_selected.save()
+            return HttpResponseRedirect('/connection')
+    else:
+        form = ConnectionForm(instance = connection_selected)
+        return render(request, 'connection/detail.html', {'form': form})
 
 @login_required
 def index(request):
-    #connection_list = Connection.objects.order_by('name')[:settings.MAX_ROWS]
     connection_list = get_authorized_connections(request)
     context = {
         'connection_list': connection_list,
     }
     return render(request, 'connection/index.html', context)
-
-
-
